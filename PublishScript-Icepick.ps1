dc978e82b2770109f440fc718a17241a882c0771
Lachie Steere <94656628+LachieSteere@users.noreply.github.com>
2025-03-12 14:51:04 +1100

    script support for laptop

--- PublishScript-Icepick.ps1
+++ PublishScript-Icepick.ps1
@@ -2,7 +2,7 @@ cd C:\Users\lachl\Documents\GitHub\LachiesLibrary
 
 robocopy F:\Dropbox\02_Personal\Notes\ObsidianVaults\LachiesMasterVault\LachiesLibrary\01_Posts C:\Users\lachl\Documents\GitHub\LachiesLibrary\content\posts /mir
 
-python3 images.py
+python3 images-Icepick.py
 
 hugo.exe
 
---
+++ PublishScript-Laptop.ps1
@@ -0,0 +1,9 @@
+cd C:\Users\lachi\Documents\GitHub\LachiesLibrary
+
+robocopy F:\Dropbox\02_Personal\Notes\ObsidianVaults\LachiesMasterVault\LachiesLibrary\01_Posts C:\Users\lachi\Documents\GitHub\LachiesLibrary\content\posts /mir
+
+python3 images-Laptop.py
+
+hugo.exe
+
+robocopy C:\Users\lachi\Documents\GitHub\LachiesLibrary\public C:\Users\lachi\Documents\GitHub\LachiesLibrary\docs /mir
